e8410a0fb13784140e77642dd0e532abfcb3a3bb
Alessandro Vannoni <alevanno@live.it>
2022-12-06 21:58:54 +0100

    add comments

--- 2022/day_5/day_5.py
+++ 2022/day_5/day_5.py
@@ -5,24 +5,23 @@ f = open("2022/day_5/input.txt", "r")
 text = f.read()
 split_text = text.split("\n\n")
 
-# Grid
+# Grid ingestion
 columns = split_text[0].split("\n")[0:-1]
 columns_split = [[line[i:i+1] for i in range(1, len(line), 4)] for line in columns]
 rotated = list(zip(*columns_split[::-1]))
 rotated_clean = [[el for el in line if el != ' '] for line in rotated]
 pprint(rotated_clean)
 
-
-# Moves
+# Moves ingestion
 moves = split_text[1].split("\n")[0:-1]
 moves_int = []
 for move_text in moves:
     move = re.findall(r'\d+', move_text)
     move = [int(a) for a in move]
+    # Adjust stack indexing
     move[1] -= 1
     move[2] -= 1
     moves_int.append(move)
-print(moves_int)
 
 # Logic
 def move_crate_9000(quantity, origin, destination, piles):
--- 2022/day_6/day_6.py
+++ 2022/day_6/day_6.py
@@ -2,11 +2,13 @@ from collections import deque
 f = open("2022/day_6/input.txt", "r")
 text = f.read()
 
+# Adjust size of the sliding window
 size = 14
 
 q = deque(maxlen=size)
 
 for i, char in enumerate(text):
+    # Check if all the elements in the sliding window are unique
     if len(set(q)) == size:
         print(i)
         break
